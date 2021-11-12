0ef51e9d0e77cbac121bce0c1c80c40a52c6b526
Aaru-verma <89626512+Aaru-verma@users.noreply.github.com>
2021-11-12 10:41:03 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -2,7 +2,8 @@ question_list = [
     "How many continents are there?",              # pehla question
     "What is the capital of India?",            # doosra question
     "NG mei kaun se course padhaya jaata hai?",    # teesra question
-    "How many colors in rainbow?"
+    "How many colors in rainbow?",
+    "What is a manchester of India?"
 ]
 
 options_list = [
@@ -13,16 +14,19 @@ options_list = [
     #third question ke liye options
     ["Software Engineering", "Counseling", "Tourism", "Agriculture"],
     #fourth question ke liye options
-    ["eight","seven","four","nine"]
+    ["eight","seven","four","nine"],
+    #fifth question ke liye options
+    ["Gujarat","Rajasthan","delhi","Ahmedabad"]
 ]
 
 # # har ek question ke liye, uski solution key (yeh index nahi hai)
-solution_list = [3, 4, 1, 2]
+solution_list = [3, 4, 1, 2, 4]
 lifeline=[
     ["(1)Four" ,"(3)Seven"],
     ["(4)Delhi" ,"(2)Chennai"],
     ["(2)Counceling","(1)Software Engineerung"],
-    ["(3)four" , "(2)seven"]
+    ["(3)four" , "(2)seven"],
+    ["(1)Gujrat" , "(4)Ahmedabad"]
 ]
 
 print("Welcome to KBC  🕺🕺 🙏 🙏")
