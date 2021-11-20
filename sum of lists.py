44d89e9a4d14f21b4babd2a44f5d8110b50d773a
Aaru-verma <89626512+Aaru-verma@users.noreply.github.com>
2021-11-20 14:50:49 +0530

    Add files via upload

---
+++ sum of lists.py
@@ -0,0 +1,71 @@
+# num=[[1,2,3],[4,5,6],[7,8,9],3,6]
+# i=0
+# s=0
+# while i<len(num):
+#     if type(num[i])==type(num):
+#         j=0
+#         while j<len(num[i]):
+#             s+=num[i][j]
+#             j+=1
+#     i+=1
+# print(s)
+# # 
+# 
+# 
+# 
+# num=list(input("enter the number:-"))
+# i=0
+# while i<len(num):
+    # if type(num)==type(num[i]):
+        # print((num[i]))
+    # i+=1 
+# print(num)
+# i=0
+# s=0
+# while i<len(num):
+    # if type(num[i])==type(num):
+        # j=0
+        # while j<len(num[i]):
+            # s+=num[i][j]
+            # j+=1
+    # i+=1
+# print(s)
+
+
+
+
+# num=[[1,2,3],[4,5,6],[3,6,8]]
+# i=0
+# sum=0
+# while i < len(num):
+#     sum+=num[i][0]
+#     i+=1
+# print(sum)
+
+
+
+# num=[[1,2,3],[4,6,6],[3,6,8]]
+# i=0
+# while i<len(num):
+# #     s+=num[i][0]
+# #     s+=num[i][1]
+# #     # j=0
+# #     # while j<len(num[i]):
+# #         # s+=num[i][0]
+# #     i+=1
+# # print(s)
+#     j=0
+#     sum=0
+#     while j<len(num[i]):
+#         if j==0:
+#            sum=sum+num[i][j] 
+#         #    print(sum)
+#         elif j==1:
+#             sum=sum+num[i][j]
+#             # print(sum)
+#         elif j==2:
+#            sum=sum+num[i][j] 
+#         #    print(sum)
+#         j=j+1
+#     i=i+1
+#     print(sum)
